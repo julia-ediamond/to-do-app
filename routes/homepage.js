496b9bb8b95afcf426de1a34998296230854603b
Julia Sharnina <>
2021-03-22 10:36:41 +1000

    Added route to delete task.

--- routes/homepage.js
+++ routes/homepage.js
@@ -27,7 +27,7 @@ router.get("/tasks", (req, res) => {
 });
 
 
-//post route
+//post task route
 router.post("/task", (req, res) => {
     db.none('INSERT INTO lists (user_id, task, date) VALUES ($1, $2, $3)'
         [req.body.userId, req.body.task, req.body.date])
@@ -42,4 +42,34 @@ router.post("/task", (req, res) => {
 			})
 		})
 })
+
+
+//delete task route 
+
+router.post('/delete', (req, res) => {
+
+    if (req.query.taskId) {
+
+        db.none('DELETE FROM lists WHERE id = $1 AND user_id = $2', [req.query.id, req.query.userId])
+            .then(() => {
+                res.redirect('/?message=Task%deleted.')
+            })
+            .catch((err) => {
+                res.render('pages/error', {
+                    title: 'Error. Please try again.',
+                    err: err
+                })
+            })
+    } else {
+        res.render('pages/error', {
+            title: 'Error',
+            err: {
+                message: 'There is no task ID found.'
+            }
+        })
+    }
+}) 
+        
+        
+
 module.exports = router;
